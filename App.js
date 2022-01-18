5fd8819e11c671c7beea2d4df192c915ec3cf18a
williamwhang <whang.william@gmail.com>
2022-01-18 18:46:58 -0300

    Alterando texto

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>React Native OK!</Text>
       <StatusBar style="auto" />
     </View>
   );
