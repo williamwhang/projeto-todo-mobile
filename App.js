2fdb485f62cb110e1971ba792486dd20d6f4db5b
williamwhang <whang.william@gmail.com>
2022-01-18 20:05:57 -0300

    tela home e estrutura de pastas e arquivos

--- App.js
+++ App.js
@@ -1,20 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Home from './src/views/Home';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>React Native OK!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Home />
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/views/Home/styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StylesSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#FFF',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+});
+
+export default styles;
