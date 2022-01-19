9c185d383aaccc9a08972e39c2ee3f9923857690
williamwhang <whang.william@gmail.com>
2022-01-19 14:12:04 -0300

    Criação do componente Footer

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -5,11 +5,13 @@ import styles from './styles';
 
 // COMPONENTES 
 import Header from '../../components/Header';
+import Footer from '../../components/Footer';
 
 export default function Home() {
     return (
         <View style={styles.container}>
-            <Header showNotification={true} showBack={false}/>
+            <Header showNotification={true} showBack={false} />
+            <Footer icon={'add'} />
         </View>
     )
 }
