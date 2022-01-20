16b9d4b99bd174d192131dfbcd675843b427ebc1
williamwhang <whang.william@gmail.com>
2022-01-20 15:45:41 -0300

    Criação dos Filtros

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
+// import React from 'react';
+// console.disableYellowBox = true;
+
 import Home from './src/views/Home';
 
 export default function App() {
