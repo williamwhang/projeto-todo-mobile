ca7c322e1c47771e0587248c54cd4afc69ec7c7d
williamwhang <whang.william@gmail.com>
2022-01-25 13:23:03 -0300

    Listando as tarefas atrasadas

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -15,6 +15,7 @@ export default function Home() {
     const [filter, setFilter] = useState('today');
     const [tasks, setTasks] = useState([]);
     const [load, setLoad] = useState(false);
+    const [lateCount, setLateCount] = useState();
 
     async function loadTasks() {
         setLoad(true);
@@ -25,13 +26,25 @@ export default function Home() {
             });
     }
 
+    async function lateVerify() {
+        await api.get(`/task/filter/late/11:11:11:11:11:11`)
+            .then(response => {
+                setLateCount(response.data.length)
+            });
+    }
+
+    function Notification() {
+        setFilter('late');
+    }
+
     useEffect(() => {
         loadTasks();
+        lateVerify();
     }, [filter])
 
     return (
         <View style={styles.container}>
-            <Header showNotification={true} showBack={false} />
+            <Header showNotification={true} showBack={false} pressNotification={Notification} late={lateCount} />
 
             <View style={styles.filter}>
                 <TouchableOpacity onPress={() => setFilter('all')}>
@@ -56,7 +69,7 @@ export default function Home() {
             </View>
 
             <View style={styles.title} >
-                <Text style={styles.titleText} > TAREFAS </Text>
+                <Text style={styles.titleText} > TAREFAS {filter == 'late' && ' ATRASADAS'} </Text>
             </View>
 
             <ScrollView style={styles.content} contentContainerStyle={{ alignItems: 'center' }}>
